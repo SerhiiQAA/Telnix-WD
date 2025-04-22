7da31183014c69ba961fe6546c106cf66f3d2896
SerhiiQA <nabukve@gmail.com>
2025-04-22 21:33:54 +0300

    run

--- test/specs/socialLinks.spec.ts
+++ test/specs/socialLinks.spec.ts
@@ -22,27 +22,21 @@ describe('Social Media Links Navigation', () => {
             name: 'Facebook',
             clickMethod: SocialPage.clickFacebookLink.bind(SocialPage),
             expectedUrl: 'facebook.com/Telnyx',
-
         },
     ];
 
     links.forEach(({ name, clickMethod, expectedUrl }) => {
         it(`Check the ${name} link in the footer`, async () => {
             await clickMethod();
-
             const handles = await SocialPage.getWindowHandles();
             expect(handles.length).toBeGreaterThan(1);
             await SocialPage.switchToWindow(handles[handles.length - 1]);
-
             await browser.waitUntil(
                 async () => (await SocialPage.getCurrentUrl()) !== 'about:blank',
                 { timeout: 10000, timeoutMsg: 'New URL did not load' }
             );
-
             const currentUrl = await SocialPage.getCurrentUrl();
-
             await SocialPage.verifyLinkidinUrl(currentUrl, expectedUrl, name);
-
             await SocialPage.closeCurrentWindow();
             await SocialPage.switchToWindow(handles[0]);
         });
